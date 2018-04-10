5c6ff3036c69e74ced8e426a5a67882b42549b01
pras75299 <singh.prashantking@gmail.com>
2018-04-10 15:18:46 +0530

    Starting of the application, add some basic variables and initialize values

--- app.js
+++ app.js
@@ -8,3 +8,17 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+// Adding variables here
+
+var activePlayer, score, roundScore;
+score = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+
+document.querySelector('.dice').style.display = 'none';
